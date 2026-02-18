2269661681a32e3e85bce258b4dd5af34d367054
Tarunkumar Reddy Thippareddy <tthippareddy1@student.gsu.edu>
2026-02-18 03:00:03 -0500

    Time: 3 ms (90.14%), Space: 22.8 MB (60.63%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        group = {}
+        for num in nums:
+            group[num] = group.get(num,0)+1
+        
+        sorted_dic = dict(sorted(group.items(), key = lambda item: item[1], reverse = True))
+        sorted_keys = list(sorted_dic.keys())
+        final = []
+        for i in range(k):
+            final.append(sorted_keys[i])
+        return final
+            
+
+
+        
+        
+
+        # values = list(group.values())
+        # values.sort(reverse = True)
+        # final = []
+        # for val in values[:k]:
+        #     final.append(group[val])
+
+
+
+        # keys = list(group.keys())
+        # final = []
+        
+        # for val in range(k):
+        #     final.append(keys[val])
+        # return final
+            
+           
